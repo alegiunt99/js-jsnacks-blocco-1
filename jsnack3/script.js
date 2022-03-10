310df2bcca3ca6636d6ad6b02f22272ad5775641
Alessandro Giunta <alegiunt99@gmail.com>
2022-03-10 18:52:00 +0100

    Ho creato un prompt per far scrivere 4 cifre all'user

---
+++ jsnack3/script.js
@@ -0,0 +1,16 @@
+console.log('js ok');
+
+
+/*Chiedi un numero di 4 cifre all’utente e 
+calcola la somma di tutte le cifre che compongono il numero. */
+
+// devo far inserire 4 numeri all'utente
+
+for (let i = 1; i < 5; i++) {
+
+    const numbersUser = parseInt(prompt('inserisci un numero'));
+
+    console.log(numbersUser);
+
+
+}
