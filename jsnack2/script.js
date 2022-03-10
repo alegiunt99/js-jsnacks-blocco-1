ef568fdb3a00741a104720aadbc5854a713add89
Alessandro Giunta <alegiunt99@gmail.com>
2022-03-10 16:20:06 +0100

    Ho creato il prompt

--- jsnack2/script.js
+++ jsnack2/script.js
@@ -1,1 +1,22 @@
 console.log("JS OK");
+
+/* Crea un array vuoto. 
+
+Chiedi per 6 volte all’utente di inserire un numero,
+
+se è dispari inseriscilo nell’array.*/
+
+const emptyArray = [];
+
+// controllo che l'utente scriva effettivamente dei numeri
+
+while (emptyArray.length < 6) {
+
+    console.log("l'array contiene:", emptyArray.length + "numeri");
+    
+    const selectedNumber = parseInt(prompt('inserisci un numero da 1 a 6'));
+
+    console.log(selectedNumber);
+
+    
+}
