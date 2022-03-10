f1c5116ef32d78d964528a9d305c26d374c9f57e
Alessandro Giunta <alegiunt99@gmail.com>
2022-03-10 18:40:31 +0100

    Ho aggiunto i numeri dispari all'array

--- jsnack2/script.js
+++ jsnack2/script.js
@@ -6,27 +6,47 @@ Chiedi per 6 volte all’utente di inserire un numero,
 
 se è dispari inseriscilo nell’array.*/
 
+//devo creare un array vuoto
+
 const emptyArray = [];
 
-// controllo che l'utente scriva effettivamente dei numeri
+console.log(emptyArray);
 
-while (emptyArray.length < 6){
+//devo far inserire 6 numeri all'utente
 
-    let numberSelected = parseInt(prompt("scrivi un numero"));
+for (let i = 1; i < 7; i++){
 
-    if (!isNan(numberSelected)){
+    let numberSelected = parseInt(prompt('Inserisci un numero'));
+
+    console.log(numberSelected);
+
+    /*e da quei numeri inseriti dall'utente
+    devo aggiungere all'array solo quelli dispari*/
+
+    let numberDis = false;
+
+    if (numberSelected % 2 === 1) {
+
+        console.log('è dispari');
+        console.log("lo aggiungo all'array");
+
+        emptyArray.push(numberSelected);
         
-        if (numberSelected % 2 === 1) {
-            emptyArray.push(numberSelected)
-
-            console.log(numberSelected, 'è dispari');
-
-            console.log("ho aggiunto un numero all'array");
-        } else (numberSelected % 2 === 0); {
-
-            console.log(numberSelected, 'è pari')
-
-            console.log("ho aggiunto nessun numero all'array");
-        }
+        numberDis = true;
     }
+
+    if (!numberDis) {
+        console.log('è pari');
+        console.log("NON lo aggiungo all'array");
+    }
+
+
 }
+
+
+
+// a questo punto sono sicuro che ha inserito 6 numeri
+
+// ora
+
+
